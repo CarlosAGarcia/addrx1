202556425c094cc76a83acdb54790988ee3b4d2c
Carlos Garcia <C@CA-GA.local>
2022-01-18 03:32:11 +0800

    remove ssr from coming soon page to make sure window is defined

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
-import ComingSoon from "../components/comingSoon/ComingSoon"
+import dynamic from 'next/dynamic'
+
+const ComingSoon = dynamic(() => import('../components/comingSoon/ComingSoon'), {
+    ssr: false
+  });
 
 const Index = () => (
     <div className='main' style={{ padding: 0, margin: 0 }}><ComingSoon/></div>
