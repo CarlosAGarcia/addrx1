94d8f362fcb1472285ca946b6e3d802634317959
Carlos <thethirdcage@gmail.com>
2022-08-29 21:11:29 +0800

    Updated git config user.email finally so i can stop ghost commits on activity

--- stores/threeStore.js
+++ stores/threeStore.js
@@ -14,6 +14,7 @@ import { get, isNumber } from 'lodash'
 let isInit = false
 let totalAnimationTime = 3.5
 
+
 // general scene variables
 let camera, controls, scene, renderer;
 let group, textMesh1, textGeo;
